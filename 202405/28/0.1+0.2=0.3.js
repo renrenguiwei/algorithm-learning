a225629a7d119724b771f45064d09f20e7de76c9
ClausWong <726787295@qq.com>
2024-05-28 11:05:27 +0800

    高精度加法、相等否

--- 202405/28/0.1+0.2=0.3.js
+++ 202405/28/0.1+0.2=0.3.js
@@ -1,23 +1,23 @@
 const Decimal = require('decimal.js')
 
-// // 1.空差值比较
-// function areEqual(num1, num2, tolerance = Number.EPSILON) {
-//     return Math.abs(num1 - num2) < tolerance;
-// }
-//
-// // 例如
-// console.log(areEqual(0.1 + 0.2, 0.3)); // true
-//
-// // 2.整数计算
-// function areEqual2(num1, num2) {
-//     const precision = 1000000; // 保留6位小数
-//     const num1Int = Math.round(num1 * precision);
-//     const num2Int = Math.round(num2 * precision);
-//     return num1Int === num2Int;
-// }
-//
-// // 例如
-// console.log(areEqual2(0.1 + 0.2, 0.3)); // true
+// 1.空差值比较
+function areEqual(num1, num2, tolerance = Number.EPSILON) {
+    return Math.abs(num1 - num2) < tolerance;
+}
+
+// 例如
+console.log(areEqual(0.1 + 0.2, 0.3)); // true
+
+// 2.整数计算
+function areEqual2(num1, num2) {
+    const precision = 1000000; // 保留6位小数
+    const num1Int = Math.round(num1 * precision);
+    const num2Int = Math.round(num2 * precision);
+    return num1Int === num2Int;
+}
+
+// 例如
+console.log(areEqual2(0.1 + 0.2, 0.3)); // true
 
 // 3.引入三方库
 function areEqual3(num1, num2) {
