29c4b8b050c163fb70a01bdad8a4f2df8243aade
ClausWong <726787295@qq.com>
2024-08-07 21:03:44 +0800

    leetcode# 寻找数组 中位数 todo: 二分查找

---
+++ 0807-Median_of_two_ordered_arrays.ts
@@ -0,0 +1,19 @@
+function findMedianSortedArrays(nums1, nums2) {
+    const newNums = [...nums1, ...nums2].sort((a, b) => a - b)
+    const len = newNums.length
+    if (len === 1) return newNums[0]
+
+    let targetNum
+    if (len%2 === 1) {
+        targetNum = newNums[(len-1)/2]
+    } else {
+        targetNum = (newNums[len/2] + newNums[(len/2)-1])/2
+    }
+    return targetNum
+}
+
+findMedianSortedArrays([-2, -1], [3])
+
+
+
+// todo 尝试利用二分法
