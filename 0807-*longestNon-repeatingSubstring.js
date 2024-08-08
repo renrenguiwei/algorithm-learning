17aafdef8d8459c1d5e4e07dc2826c30512fb8f8
ClausWong <726787295@qq.com>
2024-08-08 08:27:44 +0800

    leetcode# 字符串中无重复子字符串最长长度 思路图 调整

--- 0807-*longestNon-repeatingSubstring.js
+++ 0807-*longestNon-repeatingSubstring.js
@@ -1,6 +1,6 @@
 // 左滑窗口方法
 // 后面可以详细看
-// 思路图：https://cloud.send2boox.com/658bab3ebf84c456f0b970ac/share/%E7%AC%94%E8%AE%B01_2.png?OSSAccessKeyId=LTAI4GLDZynkeejzJembJiS6&Expires=1723129366&Signature=HXFGqgFkkXgXS3%2FQBhZ1Fr6QuoQ%3D
+// 思路图：https://cloud.send2boox.com/658bab3ebf84c456f0b970ac/share/%E7%AC%94%E8%AE%B01_3.png?OSSAccessKeyId=LTAI4GLDZynkeejzJembJiS6&Expires=1723163037&Signature=3HMN4HeqBIxTmoUome6W8b5ZiR4%3D
 function lengthOfLongestSubstring(str) {
     let left = 0
     let right = 0
@@ -9,15 +9,20 @@ function lengthOfLongestSubstring(str) {
 
     const charSet = new Set()
     while(right < str.length) {
+
         if (!charSet.has(str[right])) {
-            charSet.add(str[right])
-            right++
+            charSet.add(str[right++])
+            if (right - left > maxLength) {
+                maxStr = str.substring(left, right)
+            }
             maxLength = Math.max(maxLength, right - left)
-            maxStr = str.slice(left, right)
         } else {
-            charSet.delete(str[left])
-            left++
+            charSet.delete(str[left++])
         }
+        console.log(maxStr, left, right, charSet.values())
     }
+
     return { maxLength, maxStr }
 }
+
+console.log(lengthOfLongestSubstring('121'))
