7cec292707c026bed39fc304b4a79f75b23a17d7
ClausWong <726787295@qq.com>
2024-08-12 17:07:42 +0800

    leetcode# 简化unix路径-利用堆栈思想

--- 0807-simplify-path.js
+++ 0807-simplify-path.js
@@ -1,1 +1,23 @@
-// 简化路径
+/**
+ * @param {string} path
+ * @return {string}
+ * 利用栈的思想
+ */
+
+var simplifyPath = function(path) {
+    // /c/../a/../b/c/../../../
+    // ['', 'c', '..', 'a', '..', 'b', 'c', '..', '..', '..']
+    const pathArray = path.split('/')
+    const stack = []
+    pathArray.forEach(node => {
+        if (['', '.'].includes(node)) {     // 不做操作
+
+        } else if (['..'].includes(node)) { // 弹出尾部节点
+            stack.pop()
+        } else {
+            stack.push(node)                // 推进节点
+        }
+    })
+
+    return '/' + stack.join('/')
+}
